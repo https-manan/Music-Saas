6ccb117f3b500d1333493602a01ffa142247916e
Manan <Mananbhardwaj2705@gmail.com>
2025-10-27 17:49:36 +0530

    Added upvote BE.

---
+++ music-app/app/api/streams/upvotes/route.ts
@@ -0,0 +1,40 @@
+import {z} from 'zod'
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/app/lib/db';
+
+const UpvoteSchema = z.object({
+    streamId:z.string()
+})
+
+export async function POST(req:NextRequest){
+    const session = await getServerSession()
+    const user = await prisma.user.findFirst({where:{email: session?.user?.email ?? ""}})
+    
+    if(!user){ 
+        return NextResponse.json({
+            message:'Unauthenticated User'
+        },{
+            status:403
+        })
+    }
+
+    try {
+        const data = UpvoteSchema.parse(await req.json())
+        await prisma.upvote.create({
+            data:{
+                userId: user.id,
+                streamId: data.streamId
+            }
+        })
+        return NextResponse.json({
+            message: 'Upvoted successfully'
+        })
+    } catch (error) {
+        return NextResponse.json({
+            message:'Server error'
+        }, {
+            status: 500
+        })
+    }
+}
